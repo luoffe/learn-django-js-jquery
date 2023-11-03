c851bd1dba03b042261f89c26d3de0361d08e016
César P. <cesarlpb@gmail.com>
2023-11-03 11:31:39 +0100

    Ruta de la vista (view) 'test_view'

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import test_view, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test_view/', test_view, name='test_view'),
+    path('', index, name='index'),
 ]
